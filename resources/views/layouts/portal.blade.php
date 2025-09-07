1124c9d14fec6352bd58630d2b68dcf1452ee1c6
rizkiatnousbalpanglawa <usbalsaleko@gmail.com>
2025-09-07 15:00:48 +0800

    redesign app

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -8,7 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <style>
-        /* Video Background */
         .video-bg {
             position: fixed;
             right: 0;
@@ -19,7 +18,6 @@
             z-index: -1;
         }
 
-        /* Overlay agar teks lebih jelas */
         .overlay {
             position: fixed;
             top: 0;
@@ -55,7 +53,6 @@
             border: none;
             background: linear-gradient(to right, #fff, #E5E6E4);
             margin: 0 auto;
-            /* biar center */
             border-radius: 2px;
         }
 
@@ -82,15 +79,13 @@
             box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
         }
 
-        /* Preloader */
-        #preloader {
+        Preloader #preloader {
             position: fixed;
             top: 0;
             left: 0;
             width: 100%;
             height: 100%;
             background: #000;
-            /* hitam supaya blend sama background video */
             display: flex;
             align-items: center;
             justify-content: center;
@@ -108,63 +103,13 @@
 <body>
 
     <!-- Preloader -->
-    <div id="preloader">
+    {{-- <div id="preloader">
         <div class="spinner-border text-light" style="width: 3rem; height: 3rem;" role="status">
             <span class="visually-hidden">Loading...</span>
         </div>
-    </div>
+    </div> --}}
 
-    <!-- Background Video -->
-    <video autoplay muted loop class="video-bg">
-        <source src="{{ asset('front-assets/mamasa-bg.mp4') }}" type="video/mp4">
-        Browser anda tidak mendukung video background.
-    </video>
-    <div class="overlay"></div>
-
-    <!-- Hero Section -->
-    <div class="content hero">
-        <img src="{{ asset('front-assets/img/logo-mamasa.png') }}" alt="Logo Kabupaten Mamasa" class="mb-3"
-            style="width:150px; height:auto;">
-
-        <h1 class="display-6 fw-bold">PEMERINTAH KABUPATEN MAMASA</h1>
-        <p class="lead">Informasi | Layanan Publik | Pariwisata | Pengaduan</p>
-
-        <hr class="custom-divider my-4">
-
-
-        <section id="menu" class="container py-5 text-center">
-            <div class="row justify-content-center">
-
-                <div class="col-4 col-md-2 mb-4">
-                    <a href="{{ url('/beranda') }}" class="menu-item">
-                        <div class="icon-circle">
-                            <i class="bi bi-house-door" style="font-size: 2rem; color: white;"></i>
-                        </div>
-                        <p class="mt-2">Berita</p>
-                    </a>
-                </div>
-
-                <div class="col-4 col-md-2 mb-4">
-                    <a href="#" class="menu-item">
-                        <div class="icon-circle">
-                            <i class="bi bi-megaphone" style="font-size: 2rem; color: white;"></i>
-                        </div>
-                        <p class="mt-2">Pengumuman</p>
-                    </a>
-                </div>
-
-                <div class="col-4 col-md-2 mb-4">
-                    <a href="#" class="menu-item">
-                        <div class="icon-circle">
-                            <i class="bi bi-globe" style="font-size: 2rem; color: white;"></i>
-                        </div>
-                        <p class="mt-2">Website</p>
-                    </a>
-                </div>
-
-            </div>
-        </section>
-    </div>
+    {{ $slot }}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
--- resources/views/livewire/portal.blade.php
+++ resources/views/livewire/portal.blade.php
@@ -1,1 +1,53 @@
-<div class=""></div>
+<div class="">
+    <!-- Background Video -->
+    <video autoplay muted loop class="video-bg">
+        <source src="{{ asset('front-assets/mamasa-bg.mp4') }}" type="video/mp4">
+        Browser anda tidak mendukung video background.
+    </video>
+    <div class="overlay"></div>
+
+    <!-- Hero Section -->
+    <div class="content hero">
+        <img src="{{ asset('front-assets/img/logo-mamasa.png') }}" alt="Logo Kabupaten Mamasa" class="mb-3"
+            style="width:150px; height:auto;">
+
+        <h1 class="display-6 fw-bold">PEMERINTAH KABUPATEN MAMASA</h1>
+        <p class="lead">Informasi | Layanan Publik | Pariwisata | Pengaduan</p>
+
+        <hr class="custom-divider my-4">
+
+
+        <section id="menu" class="container py-5 text-center">
+            <div class="row justify-content-center">
+
+                <div class="col-4 col-md-2 mb-4">
+                    <a href="{{ url('/beranda') }}" class="menu-item">
+                        <div class="icon-circle">
+                            <i class="bi bi-house-door" style="font-size: 2rem; color: white;"></i>
+                        </div>
+                        <p class="mt-2">Berita</p>
+                    </a>
+                </div>
+
+                <div class="col-4 col-md-2 mb-4">
+                    <a href="#" class="menu-item">
+                        <div class="icon-circle">
+                            <i class="bi bi-megaphone" style="font-size: 2rem; color: white;"></i>
+                        </div>
+                        <p class="mt-2">Pengumuman</p>
+                    </a>
+                </div>
+
+                <div class="col-4 col-md-2 mb-4">
+                    <a href="#" class="menu-item">
+                        <div class="icon-circle">
+                            <i class="bi bi-globe" style="font-size: 2rem; color: white;"></i>
+                        </div>
+                        <p class="mt-2">Website</p>
+                    </a>
+                </div>
+
+            </div>
+        </section>
+    </div>
+</div>
