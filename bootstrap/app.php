dfff070a5579aca12cc5892e0c946528f14d2cc8
rizkiatnousbalpanglawa <usbalsaleko@gmail.com>
2025-09-18 22:10:52 +0800

    redesign layout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -10,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {})
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(TrustProxies::class);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
