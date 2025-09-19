e0748c2a0c6451de9b3684a248de09bbc4fed57e
rizkiatnousbalpanglawa <usbalsaleko@gmail.com>
2025-09-19 15:28:15 +0800

    redesign layout

--- app/Livewire/Kegiatan/Show.php
+++ app/Livewire/Kegiatan/Show.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\Kegiatan;
 
+use App\Models\Kegiatan;
 use Livewire\Component;
 
 class Show extends Component
 {
+    public $slug, $kegiatan;
+
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+        $this->kegiatan = Kegiatan::where('slug', $this->slug)->firstOrFail();
+        if (!session()->has('viewed_' . $this->kegiatan->id)) {
+            $this->kegiatan->increment('views');
+            session()->put('viewed_' . $this->kegiatan->id, true);
+        }
+    }
+
     public function render()
     {
-        return view('livewire.kegiatan.show');
+        $data['kegiatan'] = Kegiatan::with(['kategori'])->firstWhere('slug', $this->slug);
+        $data['kegiatanLain'] = Kegiatan::limit(5)->get();
+        return view('livewire.kegiatan.show', $data);
     }
 }
--- resources/views/livewire/kegiatan/show.blade.php
+++ resources/views/livewire/kegiatan/show.blade.php
@@ -30,38 +30,27 @@
                             <div class="col-md-4">
                                 <div class="meta-item">
                                     <i class="bi bi-calendar-date"></i>
-                                    <span>10/24/2023</span>
+                                    <span>{{ $kegiatan->waktu_mulai->format('d-M-Y') }}</span>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="meta-item">
                                     <i class="bi bi-clock"></i>
-                                    <span>3:00 PM - 6:00 PM</span>
+                                    <span>{{ $kegiatan->waktu_mulai->format('H:i') }} WITA</span>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="meta-item">
                                     <i class="bi bi-geo-alt"></i>
-                                    <span>Main Auditorium</span>
+                                    <span>{{ $kegiatan->tempat_kegiatan }}</span>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                     <div class="event-content" data-aos="fade-up" data-aos-delay="200">
-                        <h2>Annual Science Exhibition</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non mauris maximus, finibus
-                            dui eget, rhoncus diam. Suspendisse blandit diam at nisi rutrum, non blandit magna molestie.
-                            Cras dapibus finibus diam, eu varius purus eleifend eu. Nulla facilisi. Vestibulum ante
-                            ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
-                        </p>
-                        <p>
-                            Donec cursus, sapien vel convallis lobortis, dolor nisl pharetra est, ac facilisis ligula
-                            sapien vel justo. Curabitur sed semper risus, non tempus lorem. Nulla sodales feugiat
-                            tempus. Cras tincidunt dapibus ante, ut rutrum sapien finibus at. Mauris consequat tellus eu
-                            nunc pharetra, eu convallis elit tempor.
-                        </p>
+                        <h2>{{ $kegiatan->judul }}</h2>
+                        {!! $kegiatan->konten !!}
 
 
                     </div>
@@ -73,42 +62,20 @@
 
                         <div class="sidebar-widget related-events" data-aos="fade-left" data-aos-delay="400">
                             <h3>Kegiatan Lainnya</h3>
-                            <a href="">
-                                <div class="related-event-item">
-                                    <div class="related-event-date">
-                                        <span class="day">15</span>
-                                        <span class="month">Nov</span>
+                            @foreach ($kegiatanLain as $item)
+                                <a href="{{ route('kegiatan.detail', $item->slug) }}">
+                                    <div class="related-event-item">
+                                        <div class="related-event-date">
+                                            <span class="day">{{ $item->waktu_mulai->format('d') }}</span>
+                                            <span class="month">{{ $item->waktu_mulai->format('M') }}</span>
+                                        </div>
+                                        <div class="related-event-info">
+                                            <h4>{{ Str::limit($item->judul, 40, '...') }}</h4>
+                                            <p><i class="bi bi-geo-alt"></i> {{ $item->tempat_kegiatan }}</p>
+                                        </div>
                                     </div>
-                                    <div class="related-event-info">
-                                        <h4>Mathematics Olympiad</h4>
-                                        <p><i class="bi bi-geo-alt"></i> Room 203, East Wing</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="">
-                                <div class="related-event-item">
-                                    <div class="related-event-date">
-                                        <span class="day">05</span>
-                                        <span class="month">Dec</span>
-                                    </div>
-                                    <div class="related-event-info">
-                                        <h4>Literature Festival</h4>
-                                        <p><i class="bi bi-geo-alt"></i> Central Library</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="">
-                                <div class="related-event-item">
-                                    <div class="related-event-date">
-                                        <span class="day">18</span>
-                                        <span class="month">Dec</span>
-                                    </div>
-                                    <div class="related-event-info">
-                                        <h4>Annual Sports Meet</h4>
-                                        <p><i class="bi bi-geo-alt"></i> School Ground</p>
-                                    </div>
-                                </div>
-                            </a>
+                                </a>
+                            @endforeach
                         </div>
                     </div>
                 </div>
