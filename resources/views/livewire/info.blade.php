7e9b8082ffb28e27cd7cba0a6e5266e30684c639
rizkiatnousbalpanglawa <usbalsaleko@gmail.com>
2025-09-19 14:23:07 +0800

    redesign layout

--- resources/views/livewire/info.blade.php
+++ resources/views/livewire/info.blade.php
@@ -27,7 +27,7 @@
             </div>
 
             <div class="container my-4 my-lg-5">
-                <div class="row">
+                <div class="row custom-content-area">
                     <div class="col-lg-4 mb-4">
                         <div class="card shadow-sm border-0 rounded-4">
                             <div class="card-body p-4">
