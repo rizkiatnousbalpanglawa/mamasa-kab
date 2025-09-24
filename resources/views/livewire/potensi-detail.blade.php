5347d08b52312c56f32daed84d170d6b663c64a6
rizkiatnousbalpanglawa <usbalsaleko@gmail.com>
2025-09-24 12:37:45 +0800

    Created potensi detail

--- resources/views/livewire/potensi-detail.blade.php
+++ resources/views/livewire/potensi-detail.blade.php
@@ -20,8 +20,8 @@
                     <div class=" text-center text-muted mb-4">Ditambahkan:
                         {{ $potensi->created_at->format('d M Y H:i') }} WITA</div>
                     <div class=" text-center h3 mb-3">{{ $potensi->nama }}</div>
-                    <div class=" text-center mb-3">
-                        <img src="{{ asset(Storage::url($potensi->image)) }}" class="rounded-2 img-fluid"
+                    <div class=" text-center mb-4">
+                        <img src="{{ asset(Storage::url($potensi->image)) }}" class="rounded-2 img-responsive"
                             alt="{{ $potensi->nama }}">
 
                     </div>
