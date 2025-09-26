5281b964ffe27a65d9e5d42962b23bca3d84fd4f
rizkiatnousbalpanglawa <usbalsaleko@gmail.com>
2025-09-26 10:59:23 +0800

    redesign layout

--- resources/views/livewire/portal.blade.php
+++ resources/views/livewire/portal.blade.php
@@ -8,55 +8,57 @@
     </video>
     <div class="overlay"></div>
 
-    <!-- Hero Section -->
-    <div class="content hero">
-        <img src="{{ !empty($portals->logo) ? Storage::url($portals->logo) : asset('front-assets/img/logo-mamasa.png') }}"
-            alt="Logo Kabupaten Mamasa" class="mb-3" style="width:150px; height:auto;">
 
-        <h1 class="display-6 fw-bold">{{ $portals->judul ?? 'Pemerintah Kab Mamasa' }}</h1>
-        <p class="lead">{{ $portals->subjudul ?? '' }}</p>
+    <div class="content d-flex flex-column justify-content-center align-items-center text-center min-vh-100">
+        <div class="">
+            <img src="{{ !empty($portals->logo) ? Storage::url($portals->logo) : asset('front-assets/img/logo-mamasa.png') }}"
+                alt="Logo Kabupaten Mamasa" class="my-3" style="width:150px; height:auto;">
 
-        <hr class="custom-divider my-4">
+            <h1 class="display-6 fw-bold">{{ $portals->judul ?? 'Pemerintah Kab Mamasa' }}</h1>
+            <p class="lead">{{ $portals->subjudul ?? '' }}</p>
+
+            <hr class="custom-divider my-4">
 
 
-        <section id="menu" class="container py-5 text-center">
-            <div class="row justify-content-center">
+            <section id="menu" class="container  text-center">
+                <div class="row justify-content-center">
 
-                <div class="col-4 col-md-2 mb-4">
-                    <a href="{{ url('/beranda') }}" class="menu-item">
-                        <div class="icon-circle">
-                            <i class="bi bi-house-door" style="font-size: 2rem; color: white;"></i>
-                        </div>
-                        <p class="mt-2">Beranda</p>
-                    </a>
-                </div>
-
-                <div class="col-4 col-md-2 mb-4">
-                    <a href="#" class="menu-item" data-bs-toggle="modal" data-bs-target="#websiteOpd">
-                        <div class="icon-circle">
-                            <i class="bi bi-globe" style="font-size: 2rem; color: white;"></i>
-                        </div>
-                        <p class="mt-2">Website</p>
-                    </a>
-
-                </div>
-
-                @foreach ($menu as $item)
                     <div class="col-4 col-md-2 mb-4">
-                        <a href="{{ $item->link }}" class="menu-item">
+                        <a href="{{ url('/beranda') }}" class="menu-item">
                             <div class="icon-circle">
-                                <i class="bi bi-{{ $item->icon }}" style="font-size: 2rem; color: white;"></i>
+                                <i class="bi bi-house-door" style="font-size: 2rem; color: white;"></i>
                             </div>
-                            <p class="mt-2">{{ $item->judul }}</p>
+                            <p class="mt-2">Beranda</p>
                         </a>
                     </div>
-                @endforeach
 
-            </div>
-        </section>
+                    <div class="col-4 col-md-2 mb-4">
+                        <a href="#" class="menu-item" data-bs-toggle="modal" data-bs-target="#websiteOpd">
+                            <div class="icon-circle">
+                                <i class="bi bi-globe" style="font-size: 2rem; color: white;"></i>
+                            </div>
+                            <p class="mt-2">Website</p>
+                        </a>
+
+                    </div>
+
+                    @foreach ($menu as $item)
+                        <div class="col-4 col-md-2 mb-4">
+                            <a href="{{ $item->link }}" class="menu-item">
+                                <div class="icon-circle">
+                                    <i class="bi bi-{{ $item->icon }}" style="font-size: 2rem; color: white;"></i>
+                                </div>
+                                <p class="mt-2">{{ $item->judul }}</p>
+                            </a>
+                        </div>
+                    @endforeach
+
+                </div>
+            </section>
+        </div>
     </div>
 
-    <div class="modal modal-lg fade" id="websiteOpd" tabindex="-1" aria-labelledby="websiteOpdLabel" aria-hidden="true"
+    <div class="modal modal-xl fade" id="websiteOpd" tabindex="-1" aria-labelledby="websiteOpdLabel" aria-hidden="true"
         wire:ignore.self>
         <div class="modal-dialog">
             <div class="modal-content">
@@ -83,15 +85,15 @@
                     </div> --}}
                     <div class="row g-3">
                         @forelse ($webOpd as $item)
-                            <div class="col-lg-12 mb-1">
+                            <div class="col-lg-4 mb-1">
                                 <div class="card shadow-sm">
                                     <div class="card-body">
                                         <div class="row align-items-center">
-                                            <div class="col-1 d-flex justify-content-center">
+                                            <div class="col-2 d-flex justify-content-center">
                                                 <img src="{{ $item && $item->image ? asset(Storage::url($item->image)) : asset('front-assets/img/logo-mamasa.png') }}"
                                                     alt="Logo OPD" width="25">
                                             </div>
-                                            <div class="col">
+                                            <div class="col small">
                                                 <a href="{{ $item->url }}"
                                                     class="link-dark fw-bold text-decoration-none">
                                                     {{ $item->nama }}
