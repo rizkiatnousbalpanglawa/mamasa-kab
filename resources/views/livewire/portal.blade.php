ced5aa69b26d130a6eb2f1c5270bd55a9ef6964f
rizkiatnousbalpanglawa <usbalsaleko@gmail.com>
2025-09-29 21:19:08 +0800

    redesign layout

--- resources/views/livewire/portal.blade.php
+++ resources/views/livewire/portal.blade.php
@@ -2,7 +2,7 @@
     <!-- Background Video -->
     <video autoplay muted loop class="video-bg">
         <source
-            src="{{ !empty($portals->background) ? Storage::url($portals->background) : asset('front-assets/bg-formatted.mp4') }}"
+            src="{{ !empty($portals->background) ? Storage::url($portals->background) : asset('front-assets/unwatermark_video.mov') }}"
             type="video/mp4">
         Browser anda tidak mendukung video background.
     </video>
